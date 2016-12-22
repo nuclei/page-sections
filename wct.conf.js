8e7cd0093acecca936ed7838e4da10b7c5b69170
Lukas Oppermann <lukas@vea.re>
2016-12-22 14:53:07 +0100

    init

---
+++ wct.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "verbose": false,
+    "plugins": {
+        "local": {
+          "browsers": ["chrome","firefox"]
+      }
+    //   istanbul: {
+    //     dir: "./coverage",
+    //     reporters: ["text-summary", "lcov"],
+    //     include: [
+    //       "/src/*.js"
+    //     ],
+    //     exclude: [
+    //     ]
+    //   }
+    }
+};
+// , "firefox" , "safari"
