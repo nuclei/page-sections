21d6a2f18b26a4575cf71d1d62d24cdc1d4a9792
Lukas Oppermann <lukas@vea.re>
2017-02-07 08:55:00 +0100

    fix activation

--- src/page-section-container.js
+++ src/page-section-container.js
@@ -1,97 +1,109 @@
-/* global HTMLElement */
+/* global HTMLElement CustomEvent Event */
 (function () {
   let makeTemplate = require('./make-template')
+  let template = makeTemplate`<style>
+      :host{
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+      }
+    </style>
+    <slot></slot>
+  `
 
   class PageSections extends HTMLElement {
 
     constructor () {
-            // needs to be called first
+      // If you define a ctor, always call super() first!
+      // This is specific to CE and required by the spec.
       super()
-      var template = makeTemplate`<style>
-          :host{
-              display: flex;
-              flex-direction: column;
-              flex-wrap: nowrap;
-          }
-        </style>
-        <slot></slot>
-      `
-      // set sections initial active state to false
-      this._active = false
       // create shadowRoot
       var shadowRoot = this.attachShadow({mode: 'open'})
       // check if polyfill is used
       if (typeof ShadyCSS !== 'undefined') {
         ShadyCSS.prepareTemplate(template, 'page-sections') // eslint-disable-line no-undef
-        // add content to shadowRoot & apply css polyfill
+        // apply css polyfill
         ShadyCSS.applyStyle(this) // eslint-disable-line no-undef
       }
+      // add content to shadowRoot
       shadowRoot.appendChild(document.importNode(template.content, true))
     }
-        /**
-         * get active state of parent container
-         * @method active
-         * @return {Boolean}
-         */
-    get active () {
-      return this._active
-    }
-
+    /**
+    * @method connectedCallback
+    * @description When element is added to DOM
+     */
     connectedCallback () {
-      this._scrollEvent = this._scrollEvent.bind(this)
-      // wrap sections
-      window.addEventListener('scroll', this._scrollEvent) // bing this, so that it refers to custom element instead of window
+      let element = this
+      var fn
+      // setup scroll event to check for active elements
+      window.addEventListener('scroll', function () {
+        clearTimeout(fn)
+        fn = setTimeout(function () {
+          element.setActiveState()
+        }, 10)
+      })
       // initialize activated state
       setTimeout(function () {
-        this.setActive()
-      }.bind(this), 10)
-    }
-
-    _scrollEvent (e) {
-      clearTimeout(this._scrollEvent.fn)
-      this._scrollEvent.fn = setTimeout(function () {
-        this.setActive()
-      }.bind(this), 10)
+        element.setActiveState()
+      }, 1)
     }
     /**
-     * check if section wrapper is in viewport
+     * @method _inView
+     * @description check if element is in view
      */
     get _inView () {
       return this.getBoundingClientRect().bottom > 0 && this.getBoundingClientRect().top < window.innerHeight
     }
     /**
-     * set _active property & add/remove active attr
+     * @method setActiveState
+     * @description set _active property & add/remove active attr
      */
-    _setActiveState (state) {
-      // set _active property
-      this._active = (state === true)
-      // add or remove active attribute
-      if (this._active) {
-        this.setAttribute('active','')
-        return
-      } else {
-        this.removeAttribute('active')
-      }
-    }
-    // check if a child element is in view and set it to active
-    setActive () {
-      this._setActiveState(this._inView)
-      // if element is in view, active children
+    setActiveState () {
       if (this._inView) {
+        this._setActive()
         // Get all child elements and activate visible ones
         // stop once an inactive item follows an active item
         var elements = this.querySelectorAll('page-section')
         for (var i = 0; elements.length > i; ++i) {
-          elements[i].setActive()
+          elements[i].parent = this
+          elements[i].setActiveState()
           // abort if current element is NOT in view, but previous was in view
-          if (i > 0 && elements[i].active === false && elements[i - 1].active === true) {
+          if (i > 0 && !elements[i].hasAttribute('active') && elements[i - 1].hasAttribute('active')) {
             return
           }
         }
-        return
+      } else {
+        this._setUnactive()
       }
     }
+    /**
+     * _setActive
+     */
+    _setActive () {
+      if (this.hasAttribute('active')) return
+      // set attribute
+      this.setAttribute('active', '')
+      // Dispatch the event.
+      this.dispatchEvent(new CustomEvent('activated', { 'detail': {
+        'wasActivated': this.getAttribute('wasActivated') !== null
+      }}))
+    }
+    /**
+     * _setUnactive
+     */
+    _setUnactive () {
+      // set 'wasActivated' attribute, if element was active
+      if (this.hasAttribute('active') && !this.hasAttribute('wasActivated')) {
+        this.setAttribute('wasActivated', '')
+      }
+      // remove 'active' attribute
+      this.removeAttribute('active')
+      // Dispatch the event.
+      this.dispatchEvent(new Event('deactivated'))
+    }
   }
+  /**
+   * export for commonjs module
+   */
   module.exports = PageSections
-  // window.customElements.define('page-sections', PageSections)
 })()
--- src/page-section.js
+++ src/page-section.js
@@ -1,116 +1,99 @@
-/* global HTMLElement Event */
+/* global HTMLElement CustomEvent Event */
 (function () {
+  // function to create template-element from template tag on the fly
   let makeTemplate = require('./make-template')
+  let template = makeTemplate`<style>
+      :host{
+          display: inline-block;
+          flex: 0 1 auto;
+      }
+      :host(:not([flexible])){
+          box-sizing: border-box;
+          width: 100%;
+          min-height: 100vh;
+      }
+      :host([flexible]){
+          margin-left: 50%;
+          transform: translateX(-50%);
+      }
+    </style>
+    <slot></slot>
+  `
 
   class PageSection extends HTMLElement {
     constructor () {
-            // If you define a ctor, always call super() first!
-            // This is specific to CE and required by the spec.
+      // If you define a ctor, always call super() first!
+      // This is specific to CE and required by the spec.
       super()
-      var item = makeTemplate`<style>
-                :host{
-                    display: inline-block;
-                    flex: 0 1 auto;
-                }
-                :host(:not([flexible])){
-                    box-sizing: border-box;
-                    width: 100%;
-                    min-height: 100vh;
-                }
-                :host([flexible]){
-                    margin-left: 50%;
-                    transform: translateX(-50%);
-                }
-            </style>
-            <slot></slot>
-      `
-      // by default _active is false
-      this._active = false
       // Attach a shadow root to the element.
       var shadowRoot = this.attachShadow({mode: 'open'})
       // check if polyfill is used
       if (typeof ShadyCSS !== 'undefined') {
-        ShadyCSS.prepareTemplate(item, 'page-section') // eslint-disable-line no-undef
-        // add content to shadowRoot & apply css polyfill
+        ShadyCSS.prepareTemplate(template, 'page-section') // eslint-disable-line no-undef
+        // apply css polyfill
         ShadyCSS.applyStyle(this) // eslint-disable-line no-undef
       }
-      shadowRoot.appendChild(document.importNode(item.content, true))
+      // add content to shadowRoot
+      shadowRoot.appendChild(document.importNode(template.content, true))
     }
     /**
-     * get active state of individual section
-     * @method active
-     * @return {Boolean}
-     */
-    get active () {
-      return this._active
-    }
-    /**
-     * check if element is in view
+     * @method _inView
+     * @description check if element is in view
      */
     get _inView () {
-      // get elements bounding box
-      var box = this.getBoundingClientRect()
-      // check if element is in view
-      if ((box.top >= 0 && (
-                    box.top < window.innerHeight * 0.5 ||
-                    box.bottom <= window.innerHeight)) ||
-            (box.top < window.innerHeight * 0.5 && box.bottom > window.innerHeight * 0.5) ||
-            box.top < window.innerHeight - box.height / 1.25
-            ) {
-        return true
-      }
+      // minimum visible percent of the element to be considered active
+      var minVisible = Math.min(1, (parseFloat(this.getAttribute('requiredVisible')) || parseFloat(this.parent.getAttribute('requiredVisible')) || 0.6))
+      // px value of element that can be hidden
+      var requiredVisiblePx = minVisible * Math.min(this.getBoundingClientRect().height, window.innerHeight)
+      // calculate visible height
+      var visibleHeight = this.getBoundingClientRect().height
+      // substract part that is outside screen to top
+      visibleHeight += Math.min(0, this.getBoundingClientRect().top)
+      // substract part that is outside screen to bottom
+      visibleHeight -= Math.max(0, (this.getBoundingClientRect().bottom - window.innerHeight))
+      // return if element is visible or not
+      return visibleHeight >= requiredVisiblePx
     }
-        /**
-         * When element is added to DOM
-         */
-    connectedCallback () {
-      // initialize activated state
-      this.setActive()
-    }
-        /**
-         * set _active property & add/remove active attr
-         */
-    _setActiveState (state) {
-      if (this._active === state) {
-        return
-      }
-            // set _active property
-      this._active = (state === true)
-            // add or remove active attribute
-      if (this._active) {
-        this.setAttribute('active', '')
-        return
+    /**
+     * @method setActiveState
+     * @description set active if in viewport or unactive if not
+     */
+    setActiveState () {
+      if (this._inView) {
+        this._setActive()
       } else {
-        this.removeAttribute('active')
+        this._setUnactive()
       }
     }
     /**
-     * set active if in viewport or unactive if not
+     * _setActive
      */
-    setActive () {
-      this._setActiveState(this._inView)
-
-      if (this._inView) {
-        if (this.active === false) {
-          // set attributes
-          this.setAttribute('active', '')
-          this._active = true
-          // Dispatch the event.
-          this.dispatchEvent(new Event('activated'))
-        }
-        // return true
-        return true
+    _setActive () {
+      if (this.hasAttribute('active')) return
+      // set attribute
+      this.setAttribute('active', '')
+      // Dispatch the event.
+      this.dispatchEvent(new CustomEvent('activated', { 'detail': {
+        'wasActivated': this.getAttribute('wasActivated') !== null
+      }}))
+    }
+    /**
+     * _setUnactive
+     */
+    _setUnactive () {
+      // set 'wasActivated' attribute, if element was active
+      if (this.hasAttribute('active') && !this.hasAttribute('wasActivated')) {
+        this.setAttribute('wasActivated', '')
       }
-      // set element to unactive
-      if (this.active === true) {
-        // set attributes
-        this.removeAttribute('active')
-        this._active = false
-        // Dispatch the event.
-        this.dispatchEvent(new Event('deactivated'))
-      }
+      // remove 'active' attribute
+      this.removeAttribute('active')
+      // Dispatch the event.
+      this.dispatchEvent(new Event('deactivated'))
     }
   }
+
+  /**
+   * export for commonjs module
+   */
   module.exports = PageSection
-  // window.customElements.define('page-section', PageSectionItem)
 })()
