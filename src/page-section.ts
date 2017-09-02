13d3930a437b35ac94bf4d32dc3da7b5a7dc99f1
Lukas Oppermann <lukas@vea.re>
2017-09-02 09:24:20 +0200

    fix #content with flex-direction column

--- src/page-section.ts
+++ src/page-section.ts
@@ -24,6 +24,7 @@ let template = makeTemplate`<style>
     }
     :host([centered]) > #content{
       display: flex;
+      flex-direction: column;
       align-items: center;
       justify-content: center;
       flex: 1 1 auto;
